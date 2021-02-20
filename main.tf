316f7375892c305fa7ece57aa5f84038f813c9c6
Ethyne <dthowes@umich.edu>
2021-02-20 14:59:56 -0500

    Update main.tf
    Added my EC2 instance, and an auto selector for a CentOS AMI

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-terraform (
+terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -7,5 +7,31 @@ terraform (
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
+}
+
+data "aws_ami" "centos" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CentOS 8.* aarch64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["125523088429"] # Canonical
+}
+
+
+resource "aws_instance" "minecraft" {
+  ami = data.aws_ami.centos.id
+  instance_type = "t2.medium"
+  
+  tags = {
+    Application = "Minecraft"
+  }
 }
