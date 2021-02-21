f7a710581c2946421db19b6e492a0ad02590d95a
Ethyne <dthowes@umich.edu>
2021-02-21 15:36:16 -0500

    Update main.tf
    Try again.

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ resource "aws_security_group" "sg_minecraft" {
 }
 
 data "template_file" "user_data" {
-  template = file("github.com/Ethyne/Minecraft/scripts/cloudinit.yaml")
+  template = file("https://github.com/Ethyne/Minecraft/tree/main/scripts/cloudinit.yaml")
 }
 
 resource "aws_instance" "minecraft" {
